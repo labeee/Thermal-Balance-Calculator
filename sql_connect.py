a6929cb54bb101daa646ee47db70b58d9996a867
Zac-Milioli <zacmilioli@gmail.com>
2024-03-05 14:34:15 -0300

    uga uga

--- sql_connect.py
+++ sql_connect.py
@@ -1,21 +1,16 @@
 import sqlite3
+import pandas as pd
 
-# Connect to the database
 conn = sqlite3.connect('database.sql')
 
-# Create a cursor object
 cursor = conn.cursor()
 
-# Get the table names from the database
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = cursor.fetchall()
+# cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
+# cursor.execute("SELECT * FROM ")
+result = cursor.fetchall()
+result = pd.DataFrame(result)
 
-# Iterate over the table names
-for table in tables:
-    table_name = table[0]
+print(result)
 
-    print(f'TABLE NAME: {table_name}')
-
-# Close the cursor and connection
 cursor.close()
 conn.close()
