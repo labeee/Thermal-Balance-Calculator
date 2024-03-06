24c7b2cf83be406dba1c0ec241a8920684667f6a
Zac-Milioli <zacmilioli@gmail.com>
2024-03-06 11:02:44 -0300

    nada

--- sql_connect.py
+++ sql_connect.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect('database.sql')
 
 cursor = conn.cursor()
 
-# cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
+cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
 # cursor.execute("SELECT * FROM ")
 result = cursor.fetchall()
 result = pd.DataFrame(result)
