e34fa73d522ea548b87b23745fea3a50358f7fc6
Zac-Milioli <zacmilioli@gmail.com>
2024-03-07 14:06:21 -0300

    warnings sendo ignorados

--- system/read_db.py
+++ system/read_db.py
@@ -1,5 +1,7 @@
 import sqlite3
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 conn = sqlite3.connect(r'input/database.sql')
 
@@ -11,17 +13,23 @@ zones_dict = {}
 for i in result:
     zones_dict[i[1]] = i[0]
 print(zones_dict)
-print('-'*50)
+print('-'*80)
 
 surfaces_dict = {}
 for key, value in zones_dict.items():
     cursor.execute(f"SELECT ZoneIndex, SurfaceName, ClassName, Azimuth, ExtBoundCond FROM Surfaces WHERE ZoneIndex={value};")
     result = cursor.fetchall()
-    surfaces_dict[key] = result
-for key in zones_dict.keys():
-    df = pd.DataFrame(surfaces_dict[key], columns=['ZoneIndex', 'SurfaceName', 'ClassName', 'Azimuth', 'ExtBoundCond'])
-    print(df)
-    print('-'*50)
+    surfaces_dict[key] = pd.DataFrame(result, columns=['ZoneIndex', 'SurfaceName', 'ClassName', 'Azimuth', 'ExtBoundCond'])
+    for idx in surfaces_dict[key].index:
+        if surfaces_dict[key].at[idx, 'ExtBoundCond'] == 0:
+            surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'ext'
+        else:
+            surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'int'
+
+for key, value in surfaces_dict.items():
+    print(key)
+    print(value)
+    print('-'*80)
 
 cursor.close()
 conn.close() 
