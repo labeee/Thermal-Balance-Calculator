1ef2f823e926eae58e3e97acc026aa68721dc7f6
Zac-Milioli <zacmilioli@gmail.com>
2024-03-08 14:21:40 -0300

    gerador de zones_final

--- system/read_db.py
+++ system/read_db.py
@@ -12,8 +12,6 @@ result = cursor.fetchall()
 zones_dict = {}
 for i in result:
     zones_dict[i[1]] = i[0]
-print(zones_dict)
-print('-'*80)
 
 surfaces_dict = {}
 for key, value in zones_dict.items():
@@ -21,15 +19,44 @@ for key, value in zones_dict.items():
     result = cursor.fetchall()
     surfaces_dict[key] = pd.DataFrame(result, columns=['ZoneIndex', 'SurfaceName', 'ClassName', 'Azimuth', 'ExtBoundCond'])
     for idx in surfaces_dict[key].index:
+
         if surfaces_dict[key].at[idx, 'ExtBoundCond'] == 0:
             surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'ext'
         else:
             surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'int'
+            
+        azimuth = surfaces_dict[key].at[idx, 'Azimuth']
+        if azimuth < 22.5 or azimuth >= 337.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'north'
+        elif azimuth >= 22.5 and azimuth < 67.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'northeast'
+        elif azimuth >= 67.5 and azimuth < 112.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'east'
+        elif azimuth >= 112.5 and azimuth < 157.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'southeast'
+        elif azimuth >= 157.5 and azimuth < 202.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'south'
+        elif azimuth >= 202.5 and azimuth < 247.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'southwest'
+        elif azimuth >= 247.5 and azimuth < 292.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'west'
+        elif azimuth >= 292.5 and azimuth < 337.5:
+            surfaces_dict[key].at[idx, 'Azimuth'] = 'northwest'
 
+        match surfaces_dict[key].at[idx, 'ClassName']:
+            case 'Door':
+                surfaces_dict[key].at[idx, 'ClassName'] = 'Wall'
+            case 'Ceiling':
+                surfaces_dict[key].at[idx, 'ClassName'] = 'Roof'
+
+zones_final = {}
 for key, value in surfaces_dict.items():
-    print(key)
-    print(value)
-    print('-'*80)
+    for idx in value.index:
+        zones_final[value.at[idx, 'SurfaceName']] = f"{value.at[idx, 'Azimuth']}_{value.at[idx, 'ExtBoundCond']}{value.at[idx, 'ClassName']}"
+
+zones_final = pd.DataFrame([zones_final])
+zones_final.to_csv('zones_final.csv', index=False)
+print(zones_final)
 
 cursor.close()
 conn.close() 
