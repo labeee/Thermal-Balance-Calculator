d4fb5311b3314daa7b3771303095ae9987496780
Zac-Milioli <zacmilioli@gmail.com>
2024-03-14 13:10:15 -0300

    Melhoria na função de busca do sql

--- system/source.py
+++ system/source.py
@@ -2,13 +2,83 @@ import pandas as pd
 import warnings
 import sqlite3
 from glob import glob
-from system.read_db import *
 import os
 from datetime import datetime, timedelta
 warnings.filterwarnings("ignore")
 
+zone_addons = [
+    ''
+]
 
-# Columns per zone
+def read_db(selected_zones: list = None):
+    conn = sqlite3.connect(r'input/database.sql')
+
+    cursor = conn.cursor()
+
+    if selected_zones != None:
+        selected_zones = ', '.join(f'"{zona}"' for zona in selected_zones)
+        cursor.execute(f"SELECT ZoneIndex, ZoneName FROM Zones WHERE ZoneName IN ({selected_zones});")
+    else:
+        cursor.execute("SELECT ZoneIndex, ZoneName FROM Zones;")
+    result = cursor.fetchall()
+    zones_dict = {}
+    for i in result:
+        zones_dict[i[1]] = i[0]
+
+    surfaces_dict = {}
+    for key, value in zones_dict.items():
+        cursor.execute(f"SELECT ZoneIndex, SurfaceName, ClassName, Azimuth, ExtBoundCond FROM Surfaces WHERE ZoneIndex={value};")
+        result = cursor.fetchall()
+        surfaces_dict[key] = pd.DataFrame(result, columns=['ZoneIndex', 'SurfaceName', 'ClassName', 'Azimuth', 'ExtBoundCond'])
+        for idx in surfaces_dict[key].index:
+
+            if surfaces_dict[key].at[idx, 'ExtBoundCond'] == 0:
+                surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'ext'
+            else:
+                surfaces_dict[key].at[idx, 'ExtBoundCond'] = 'int'
+                
+            azimuth = surfaces_dict[key].at[idx, 'Azimuth']
+            if azimuth < 22.5 or azimuth >= 337.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'north'
+            elif azimuth >= 22.5 and azimuth < 67.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'northeast'
+            elif azimuth >= 67.5 and azimuth < 112.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'east'
+            elif azimuth >= 112.5 and azimuth < 157.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'southeast'
+            elif azimuth >= 157.5 and azimuth < 202.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'south'
+            elif azimuth >= 202.5 and azimuth < 247.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'southwest'
+            elif azimuth >= 247.5 and azimuth < 292.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'west'
+            elif azimuth >= 292.5 and azimuth < 337.5:
+                surfaces_dict[key].at[idx, 'Azimuth'] = 'northwest'
+
+            match surfaces_dict[key].at[idx, 'ClassName']:
+                case 'Door':
+                    surfaces_dict[key].at[idx, 'ClassName'] = 'Wall'
+                case 'Ceiling':
+                    surfaces_dict[key].at[idx, 'ClassName'] = 'Roof'
+    cursor.close()
+    conn.close()
+
+    dicionario = {}
+
+    # dicionario = {
+    #     'SALA': {
+    #         'conection': [
+                
+    #         ],
+    #         'surface': [
+
+    #         ]
+    #     }
+    # }
+    return dicionario
+
+
+# Columns per zone  
 sala = {
     "ZONE": "SALA",
     "convection": {
