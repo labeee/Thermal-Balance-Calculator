c4891f3f8b5923cbe3f3a4372967ccb5043f17f8
Zac-Milioli <zacmilioli@gmail.com>
2024-05-09 12:57:24 -0300

    primeiras tentativas de plotar (falhas)

--- plotting_space/plotter.py
+++ plotting_space/plotter.py
@@ -1,3 +1,13 @@
 import seaborn as sns
+import pandas as pd
 
+myzone = 'DORM1'
 
+df = pd.read_csv("plotting_space/anual_conv.csv", sep=',', index_col=0)
+
+zoned = df.loc[df['zone'] == myzone]
+title = f'Heatmap para zona {myzone}'
+
+mapa = sns.heatmap(data=zoned)
+mapa.figure.set_title(title)
+sns.plt.show()
